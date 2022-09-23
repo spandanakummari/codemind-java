f4767c230a2fa1e8ed7faf0d438d9bfe6cb2d5fc
spandanakummari <84307369+spandanakummari@users.noreply.github.com>
2022-09-23 15:14:18 +0530

    import java.util.Scanner; class Sample {     public static void main(String args[])     {         int n,x[],i,sum=0;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         x=new int[n];         for(i=0;i<n;i++)           x[i]=sc.nextInt();          for(i=0;i<n;i++)          {              if(i%2!=0)              {                  sum=sum+x[i];              }          }          System.out.println(sum);     } }

---
+++ Sum_of_Odd_Positions.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class Sample
+{
+    public static void main(String args[])
+    {
+        int n,x[],i,sum=0;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        x=new int[n];
+        for(i=0;i<n;i++)
+          x[i]=sc.nextInt();
+         for(i=0;i<n;i++)
+         {
+             if(i%2!=0)
+             {
+                 sum=sum+x[i];
+             }
+         }
+         System.out.println(sum);
+    }
+}
